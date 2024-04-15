ca283aaf195d662dd6df354ee6c8e2c5683fbbbf
songsz <song.shangzhi3@neolink.com>
2024-04-15 10:06:25 +0800

    feat: finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import {ChangeEventHandler, useCallback, useEffect, useState} from "react";
-import {CustomIcon, getIconData} from "@/utils/iconHelper";
+import {getIconCode, getIconData} from "@/utils/iconHelper";
 import {copyToClipboard} from "@/utils/copyToClipboard";
 
 // 定义事件对象类型
@@ -9,7 +9,7 @@ interface ChangeEvent extends Event {
 }
 
 export default function Home() {
-  const [svg, setSvg] = useState<string>('{"name":"temp","data":"<path d=\\"M637 443H519V309c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8v134H325c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h118v134c0 4.4 3.6 8 8 8h60c4.4 0 8-3.6 8-8V519h118c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8zm284 424L775 721c122.1-148.9 113.6-369.5-26-509-148-148.1-388.4-148.1-537 0-148.1 148.6-148.1 389 0 537 139.5 139.6 360.1 148.1 509 26l146 146c3.2 2.8 8.3 2.8 11 0l43-43c2.8-2.7 2.8-7.8 0-11zM696 696c-118.8 118.7-311.2 118.7-430 0-118.7-118.8-118.7-311.2 0-430 118.8-118.7 311.2-118.7 430 0 118.7 118.8 118.7 311.2 0 430z\\" />"}');
+  const [svg, setSvg] = useState<string>('');
   const [notification, setNotification] = useState<string>('');
   useEffect(() => {
     if (!notification) return
@@ -20,13 +20,14 @@ export default function Home() {
   const onChange: ChangeEventHandler<HTMLInputElement> = useCallback((event) => {
     const file = event.target.files?.[0];
     const reader = new FileReader();
+
     reader.onload = (e) => {
       const content = e?.target?.result;
-      console.log("File content:", content);
       // 在这里可以对文件内容进行处理，比如展示在页面上
       const data = getIconData('temp', content as string)
       setSvg(data);
-      copyToClipboard(data)
+      copyToClipboard(getIconCode(data))
+      setNotification('复制代码成功')
     };
     reader.onerror = (e) => {
       console.error("Error reading file:", e);
@@ -35,30 +36,38 @@ export default function Home() {
       reader.readAsText(file);
     }
   }, [setSvg])
-  console.log('iii', svg)
   return (
     <main className="flex min-h-screen flex-col items-center justify-items-start p-24">
-      <h1 className="text-2xl">选择 .svg 文件, 自动转换成自定义 icon 的格式,并复制到剪切板</h1>
+      <h1 className="text-l">选择 .svg 文件, 自动转换成 @tenx-ui/icon CustomIcon 代码,并自动复制到剪切板</h1>
       {
-        Boolean(notification) && <div className="absolute top-0 text-green-400">{notification}</div>
+        Boolean(notification) && <div className="fixed border-solid border border-blue-400 p-3 rounded-2xl top-3 text-green-400">{notification}</div>
       }
       <br/><br/>
       <input className="" type="file" accept="image/svg+xml" onChange={onChange}/>
       <br/><br/>
-      {
-        Boolean(svg) && <CustomIcon data={svg}/>
-      }
       <div>
         <textarea style={{
           width: '800px',
-        }} className="w-full bg-black text-white border-white border-solid border-2 rounded-lg" rows={10} value={svg}/>
+        }} className="w-full bg-black text-white border-white border-solid border-2 rounded-lg" rows={10} value={getIconCode(svg)}/>
       </div>
       <div>
         <br/>
-        <button onClick={() => {
-          copyToClipboard(svg)
-          setNotification('复制成功')
-        }} disabled={!svg}>Copy!
+        <button
+          className="border border-emerald-900 pt-2 pb-2 pl-5 pr-5 rounded-md"
+          onClick={() => {
+            copyToClipboard(getIconCode(svg))
+            setNotification('复制代码成功')
+          }}
+          disabled={!svg}>复制
+        </button>
+        <span >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+        <button
+          className="border border-emerald-900 pt-2 pb-2 pl-5 pr-5 rounded-md"
+          onClick={() => {
+            copyToClipboard(svg)
+            setNotification('复制 data 数据成功')
+          }}
+          disabled={!svg}>仅复制 data 数据
         </button>
       </div>
     </main>
--- src/utils/iconHelper.tsx
+++ src/utils/iconHelper.tsx
@@ -19,60 +19,7 @@ export function getIconData(name: string, svgData: string) {
   });
 }
 
-interface IIcon {
-
+export const getIconCode = (svgData: string) => {
+  return `// import { CustomIcon } from '@tenx-ui/icon';
+<CustomIcon data={'${svgData}'}/>`
 }
-const Icon: FC<IIcon> = (props) => {
-  let {
-    _type, children, style = {}, size, color, spin, className,
-    component: Component, ...other
-  } = props;
-  Component = Component || 'svg';
-  const svgProps = {};
-  if (Component === 'svg') {
-    svgProps.viewBox = '0 0 1024 1024';
-    svgProps.preserveAspectRatio = 'xMidYMid slice';
-  }
-  const _style = JSON.parse(JSON.stringify(style))
-  if (size) {
-    _style.fontSize = size
-  }
-  if (color) {
-    _style.fill = color
-  }
-
-  const classString = ['tenx-icon', _type ? `tenx-icon-${_type}` : '', spin ? 'tenx-icon-spin' : '', className || ''].filter(Boolean).join(' ')
-
-  return (
-    <i className="inline-block fs">
-      <Component
-        className="tenx-icon-svg"
-        style={_style}
-        aria-hidden="true"
-        {...svgProps}
-        {...other}
-      >
-        {children}
-      </Component>
-    </i>
-  );
-}
-
-Icon.defaultProps = {
-  component: 'svg',
-};
-
-export default Icon;
-
-export const CustomIcon = props => {
-  const { data, ...res } = props;
-  if(!data) {
-    console.warn(
-      'CustomIcon\'s data is null'
-    )
-    return null
-  }
-
-  const _data = JSON.parse(data);
-  return <Icon _type={data.name} component="svg" dangerouslySetInnerHTML={{ __html: _data.data }} {...res} />;
-};
